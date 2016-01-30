529fa50743b6d57e3a64901d0facc09d0030a81e
Nikolai Pashkevich <kulex4@gmail.com>
2016-01-30 22:53:33 +0300

    delete auto controller, some polish

--- src/main/java/com/bsuir/controller/MainController.java
+++ src/main/java/com/bsuir/controller/MainController.java
@@ -4,16 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.Map;
-
 @Controller
 public class MainController {
 
     @RequestMapping(value = { "/", "/home" }, method = RequestMethod.GET)
-    public String mainPage(Map<String, Object> model) {
-        /*model.put("time", new Date());
-        String message = "Hello World";
-        model.put("message", message);*/
+    public String mainPage() {
         return "main";
     }
 
